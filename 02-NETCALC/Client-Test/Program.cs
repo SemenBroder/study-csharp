b6dee44f4bb22ef0fe3b2716eb24cfdc900b89d7
SemenBroder <semen_kanatohodec@inbox.ru>
2019-11-07 22:27:55 +0400

    Обновление от 07.11.19

---
+++ 02-NETCALC/Client-Test/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ClientCSharp
+{
+    class TCP_Client
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Int32 port = 12345;//порт сервера
+                Console.WriteLine("Введите команду в формате CALC <+,-,*,/>, <x>, <y>");
+                string message = Convert.ToString(Console.ReadLine());
+                TcpClient client = new TcpClient("localhost", port);
+
+                //преобразуем строчку в массив байт
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // вводим поток stream для чтения и записи через установленное соединение                
+                NetworkStream stream = client.GetStream();
+
+                // посылаем сообщение серверу 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);//печатаем то, что отправили
+
+                // буффер для приема сообщений
+                data = new Byte[1000];
+
+                // строка для приема сообщений сервера
+                String responseData;
+
+                // получаем сообщение от сервера
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                //печатаем то, что получили
+                Console.WriteLine("Received: {0}", responseData);
+
+                // закрываем соединение
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException expt)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", expt);
+            }
+            catch (SocketException expt)
+            {
+                Console.WriteLine("SocketException: {0}", expt);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ 02-NETCALC/Server-Client-Test/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCP_Server
+{
+    public static void Main()
+    {
+        TcpListener server = null;
+        try
+        {
+            Int32 port = 12345; //порт сервера
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");//ip-адрес сервера (интерфейс)
+
+            //TcpListener - класс TCP-сервера из .Net Framework Class Library
+            server = new TcpListener(localAddr, port);
+
+            // начинаем ожидание подсоединений клиентов на интерфейсе localAddr и порту port
+            server.Start();
+
+            // буффер для приема сообщений и соответствующая ему строка для вывода на экран
+            Byte[] bytes = new Byte[1000];
+            String data;
+
+            //ответ клиенту
+            String answer_message;
+
+            //цикл обработки подсоединений клиентов
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+                // Ждем соединения клиента
+                TcpClient client = server.AcceptTcpClient();
+                //Ура! Кто-то подсоединился!
+                Console.WriteLine("Connected!");
+                // вводим поток stream для чтения и записи через установленное соединение
+                NetworkStream stream = client.GetStream();
+                int i = stream.Read(bytes, 0, bytes.Length);
+                if (i > 0)
+                {
+                    // преобразуем принятые данные в строку ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    //печатаем то, что получили
+                    Console.WriteLine("Received: {0}", data);
+                    //анализируем запрос клиента и вычисляем результат
+
+                   
+
+
+                   
+                    if (data.Contains("CALC"))
+                    {
+                        
+                        string[] mystring = data.Split(' ');
+                        Double x = new Double();
+                        Double y = new Double();
+
+                        x = Convert.ToDouble(mystring[2]);
+                        y = Convert.ToDouble(mystring[3]);
+                        switch (mystring[1])
+                        {
+                            case "+":
+                                answer_message = Convert.ToString(x + y);
+
+                                break; 
+
+                            case "-":
+                                answer_message = Convert.ToString(x - y);
+
+                                break;
+
+                            case "/":
+                                answer_message = Convert.ToString(x / y);
+
+                                break;
+
+                            case "*":
+                                answer_message = Convert.ToString(x * y);
+
+                                break;
+
+                            default:
+                               answer_message = "ERROR";
+                                break;
+                        }
+                    }
+                    else
+                    {
+
+                        answer_message = "ERROR";
+
+
+                    }
+
+                    
+                    //преобразуем строчку-ответ сервера в массив байт
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(answer_message);
+                    // отправляем ответ
+                    stream.Write(msg, 0, msg.Length);
+                }
+
+                // закрываем соединение
+                client.Close();
+            }
+        }
+        catch (SocketException expt)
+        {
+            Console.WriteLine("SocketException: {0}", expt);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.ReadKey();
+    }
+}
